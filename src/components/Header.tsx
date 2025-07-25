83df270a980bdbc9823b97937d109e7273fb34e1
Subhradeep1708 <subhradeep1708@gmail.com>
2025-07-25 21:40:15 +0530

    feat: update contact page with contact details and improve layout; modify header to include logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import Nav from "./Nav";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
     return (
-        <header className="fixed top-0 right-0 left-0 z-50 h-20 bg-white font-semibold">
+        <header className="relative z-50 h-24 flex items-center  font-semibold">
             <div
                 className="text-greenish flex h-full items-center justify-between"
                 id="header"
             >
-                <p>Logo</p>
+                <Link href="/" >
+
+                    <Image
+                        src="/logo.webp"
+                        alt="Logo"
+                        width={85}
+                        height={85}
+                    />
+                </Link>
                 <Nav />
             </div>
         </header>
