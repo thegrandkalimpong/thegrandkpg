23297e0c98066ebd375f1be44f1a0afd3d071ce8
Sanchari Mandal <sancharim2233@gmail.com>
2025-07-28 12:43:29 +0530

    feat: update next.config.ts to set output and basePath for project configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    output: "export",
+    basePath: "/thegrandk",
 };
 
 export default nextConfig;
