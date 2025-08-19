2cc7e29c2115fb2fe2e14f769f6a3422359bf3b1
Sanchari Mandal <sancharim2233@gmail.com>
2025-08-19 15:37:42 +0530

    feat: implement testimonials feature with dynamic data and enhance TestimonialCard component

---
+++ src/data/testimonials.ts
@@ -0,0 +1,58 @@
+export type ITestimonial = {
+        name: string;
+        // role: string;
+        content: string;
+        via: string;
+        country?: string;
+    };
+
+export const testimonials: ITestimonial[] = [
+    {
+        name: "Jonathan",
+        country: "United Kingdom",
+        via: "Booking.com",
+        content:
+            "Everything was excellent. It's a new luxurious hotel. The staff were friendly and kind, the room was luxurious with a wonderful smart TV. The food was great value. It was nice to have breakfast on my balcony. Walking from Kalimpong the hotel is just round the corner from where Google maps says it is.",
+    },
+    {
+        name: "Nupur Sengupta",
+        country: "India",
+        via: "Google",
+        content:
+            "Visited here few days before, we had a wonderful experience. Located in serene and peaceful place with all amenities. Stay was made more special by excellent behavior and care by Sharma uncle, aunty, Ashish bhai and Bunu.",
+    },
+    {
+        name: "Albert",
+        country: "India",
+        via: "Booking.com",
+        content:
+            "Excellent new, recently built country villa on the outskirts of the city, located in the middle of the Himalayan forest and bamboo thickets. Spacious, cozy rooms with all the amenities, very pleased with the perfect cleanliness, which, in general, is a rarity for India. Our rooms had spacious balconies, where it is pleasant to drink tea or coffee and enjoy the friendly singing of birds and the chirping of cicadas at night. Friendly, very caring staff, respectful of guests. We stayed only one night, ordered dinner from the menu, which turned out to be very tasty, breakfast was included in the room rate. This was the best place for our vacation during our entire 10-day trip to West Bengal and Sikkim!",
+    },
+    {
+        name: "Priyanshu Bhattecharjee",
+        country: "India",
+        via: "Google",
+        content:
+            "The Grand Kalimpong is situated in a serene area and it is quite, peaceful and surrounded by nature. It offers tons of privacy and connectivity with nature. The staff is extremely welcoming and helpful. Shiwangi from reception went above and beyond to make our stay at the Grand Kalimpong comfortable and enjoyable. She is extremely helpful, friendly and made our full Kalimpong tour easier for us to navigate. Pawan is also extremely helpful, friendly and even took one of our members to the market driving his scooty at evening. The food here is delicious and breakfast is good.",
+    },
+    {
+        name: "Rajarshi Das",
+        country: "India",
+        via: "Google",
+        content:
+            "I have been to this place a few days back, it was nice property to stay, you'll get all the required amenities, it has private parking, and the kalimpong market is very close to the place as well. The behavior of the staff is also very good and helpful 👍. A must visit",
+    },
+    {
+        name: "Dhana Sekar",
+        country: "India",
+        via: "Google",
+        content: "The location is little outside the city. Calm and Peaceful",
+    },
+    {
+        name: "Oishika Ganguly",
+        country: "India",
+        via: "Google",
+        content:
+            "This hotel provides a special connectivity with nature... Distant from crowdy areas it will help you to find the peace. All the staffs including prateek and Asish are very welcoming. They helped us in every way possible. Especially Shivani di and her hospitality.. melted our heart. Would highly recommend.",
+    },
+];
